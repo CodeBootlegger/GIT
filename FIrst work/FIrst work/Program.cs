b414c7fb35db7ad68659a54a26d5fe50780a734e
CodeBootlegger <brozzers666@gmail.com>
2018-05-22 15:29:42 +0300

    v1

--- FIrst work/FIrst work/Program.cs
+++ FIrst work/FIrst work/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,7 @@ namespace FIrst_work
     {
         static void Main(string[] args)
         {
+          Console.WriteLine("Опача");  
         }
     }
 }
